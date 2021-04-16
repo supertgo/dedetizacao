483b9c5df9e1be5d7c511faf7e532b80483f623b
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-04-16 16:57:53 -0300

    codando formulário de cadastro de orçamentos

---
+++ servidor/formularioCadastrar.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Cadastro de processos - Resolve Antimultas</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="../estilos/bulma.min.css" />
+</head>
+
+<body>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Cadastro de orçamentos/clientes</h3>
+                    <?php
+                    if(isset($_SESSION['status_cadastro'])):
+                        ?>
+                        <div class="notification is-success">
+                          <p>Cadastro efetuado!</p>
+                      </div>
+                      <?php
+                  endif;
+                  unset($_SESSION['status_cadastro']);
+                  ?>
+                  <p style="color: black"><a href="painel.php" style= "text-decoration: underline;">Voltar à zona do Administrador</a></p>
+              </div>
+
+              <?php
+              if(isset($_SESSION['usuario_existe'])):
+                ?>
+
+                <div class="notification is-info">
+                    <p>O CPF cadastrado já existe, tente novamente.</p>
+                </div>
+                <?php
+            endif;
+            unset($_SESSION['usuario_existe']);
+            ?>
+
+            <div class="box" style="max-width: 350px; display: block; margin: auto">
+                <form action="scriptCadastrar.php" method="POST" enctype="multipart/form-data">
+                    <label>Nome do cliente</label>
+                    <div class="field">
+                        <div class="control"> <!-- nome do cliente -->
+                            <input name="nome" type="text" class="input is-large" autofocus required>
+                        </div>
+                    </div>
+                    <label>CPF</label>
+                    <div class="field">
+                        <div class="control"> <!-- CPF -->
+                            <input name="cpf" type="text" class="input is-large" required> 
+                        </div>
+                    </div>
+                    <label>Telefone</label>
+                    <div class="field">
+                        <div class="control"> <!-- Endereço -->
+                            <input name="telefone" class="input is-large" required>
+                        </div>
+                    </div>
+                    <label>Email</label> <!-- Email -->
+                    <div class="field"> 
+                        <div class="control">
+                            <input name="email" class="input is-large" type="text">
+                      </div>
+                  </div>
+                  <label>Valor do orçamento</label>
+                  <div class="field"> 
+                    <div class="control"> <!-- valor -->
+                        <input name="valor" class="input is-large" required>
+                    </div>
+                </div>
+                <label>Detalhes do orçamento</label>
+                <div class="field">
+                    <div class="control"> <!-- detalhes do orçamento -->
+                        <textarea name="detalhes" class="input is-large" style="width: 350px; height: 150px;">
+                        </textarea>
+                    </div>
+                </div>
+                </div>
+                <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</section>
+<script src="https://kit.fontawesome.com/5055cfbe4f.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ servidor/painel.php
@@ -0,0 +1,25 @@
+<?php
+ include('$conexao.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/bulma.min.css">
+    <link rel="stylesheet" href="../estilos/painel.css">
+    <title>Painel de orçamentos</title>
+</head>
+<body>
+
+<div class="container-titulos-painel">
+    <h1>Olá Administrador!<h1>
+    <h1>Orçamentos Dedetização Ltda<h1>
+    <a href="./admin.php">sair</a>    
+</div>
+
+<a href="./formularioCadastrar.php"><button>Cadastrar novo orçamento</button></a>
+</body>
+</html>
